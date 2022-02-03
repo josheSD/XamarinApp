21b74165682e707fed15ff33415b797e8479ee23
Jose Chutas Ramos <josechutas.developer@gmail.com>
2022-02-03 17:57:43 -0500

    design 100

--- BcpDesign/BcpDesign.Android/obj/Debug/110/android/environment.x86_64.s
+++ BcpDesign/BcpDesign.Android/obj/Debug/110/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"036a38f4-f287-4e28-ab45-053dbe8f9067"
+	.asciz	"93f3af41-ac6a-4664-8d44-9105d5272fe5"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
